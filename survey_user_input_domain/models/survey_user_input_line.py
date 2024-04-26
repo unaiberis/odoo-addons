a18581d6ec8a8f51416c27a139407ac17ebf0004
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-26 13:24:06 +0200

    [IMP] Add loggers

--- survey_user_input_domain/models/survey_user_input_line.py
+++ survey_user_input_domain/models/survey_user_input_line.py
@@ -20,29 +20,7 @@ class SurveyUserInputLine(models.Model):
         store=True 
     )
 
-import logging
-from odoo import models, fields, api
-
-_logger = logging.getLogger(__name__)
-
-class SurveyUserInputLine(models.Model):
-    _inherit = 'survey.user_input.line'
-
-    calculated_matrix_question_id = fields.Many2one(
-        'survey.question.answer', 
-        string='Matrix Question ID',
-        compute='_compute_matrix_question_id',
-        store=True 
-    )
-    
-    calculated_suggested_answer_question_id = fields.Many2one(
-        'survey.question.answer', 
-        string='Suggested Answer ID',
-        compute='_compute_suggested_answer_id',
-        store=True 
-    )
-
-    def init(self):
+    def initialize_survey_user_input_line(self):
         _logger.info("2024okdeb - Initializing SurveyUserInputLine model")
         self._compute_matrix_question_id()
         self._compute_suggested_answer_id()
@@ -56,9 +34,12 @@ class SurveyUserInputLine(models.Model):
                     _logger.info(f"2024okdeb - Changing calculated_matrix_question_id for line {line.id} to {new_value}")
                     line.calculated_matrix_question_id = new_value
             else:
-                if line.calculated_matrix_question_id:
-                    _logger.info(f"2024okdeb - Resetting calculated_matrix_question_id for line {line.id} to False")
-                line.calculated_matrix_question_id = False
+                self._reset_calculated_matrix_question_id(line)
+
+    def _reset_calculated_matrix_question_id(self, line):
+        if line.calculated_matrix_question_id:
+            _logger.info(f"2024okdeb - Resetting calculated_matrix_question_id for line {line.id} to False")
+        line.calculated_matrix_question_id = False
 
     @api.depends('suggested_answer_id')
     def _compute_suggested_answer_id(self):
@@ -69,6 +50,9 @@ class SurveyUserInputLine(models.Model):
                     _logger.info(f"2024okdeb - Changing calculated_suggested_answer_question_id for line {line.id} to {new_value}")
                     line.calculated_suggested_answer_question_id = new_value
             else:
-                if line.calculated_suggested_answer_question_id:
-                    _logger.info(f"2024okdeb - Resetting calculated_suggested_answer_question_id for line {line.id} to False")
-                line.calculated_suggested_answer_question_id = False
+                self._reset_calculated_suggested_answer_question_id(line)
+
+    def _reset_calculated_suggested_answer_question_id(self, line):
+        if line.calculated_suggested_answer_question_id:
+            _logger.info(f"2024okdeb - Resetting calculated_suggested_answer_question_id for line {line.id} to False")
+        line.calculated_suggested_answer_question_id = False

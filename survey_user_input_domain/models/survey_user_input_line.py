fd21cdc5f1a7e092230fff28627136c0a57915a6
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-26 12:52:01 +0200

    [IMP] Improve domain

--- survey_user_input_domain/models/survey_user_input_line.py
+++ survey_user_input_domain/models/survey_user_input_line.py
@@ -3,24 +3,21 @@ from odoo import models, fields, api
 class SurveyUserInputLine(models.Model):
     _inherit = 'survey.user_input.line'
 
-    # Define el campo calculado
     calculated_matrix_question_id = fields.Many2one(
         'survey.question.answer', 
         string='Matrix Question ID',
         compute='_compute_matrix_question_id',
-        store=True  # Almacenar el valor calculado en la base de datos
+        store=True 
     )
     
-    # Define el campo calculado
     calculated_suggested_answer_question_id = fields.Many2one(
         'survey.question.answer', 
         string='Suggested Answer ID',
         compute='_compute_suggested_answer_id',
-        store=True  # Almacenar el valor calculado en la base de datos
+        store=True 
     )
 
 
-    # Método de cálculo para el campo calculado
     @api.depends('matrix_row_id')
     def _compute_matrix_question_id(self):
         for line in self:
@@ -30,7 +27,6 @@ class SurveyUserInputLine(models.Model):
                 line.calculated_matrix_question_id = False
 
 
-    # Método de cálculo para el campo calculado
     @api.depends('suggested_answer_id')
     def _compute_suggested_answer_id(self):
         for line in self:

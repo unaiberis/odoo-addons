0296f907bc303b7c221730ce33709f052dfaeea8
Unai Beristain <unaiberis@gmail.com>
2024-06-13 11:39:35 +0200

    [ADD] Improve views

--- sale_order_report/__manifest__.py
+++ sale_order_report/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Custom Sale Order Report",
     "version": "1.0",
+    "license": "AGPL-3",
     "summary": "Customize Sale Order Report in Odoo 16",
     "description": "Module to customize the Sale Order report in Odoo version 16.",
     "author": "Your Name",

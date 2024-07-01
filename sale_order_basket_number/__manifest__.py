4fe302c3edde7eced80f1f5b6d83f0fe2b601708
avanzosc <avanzosc@Avanzosc.myguest.virtualbox.org>
2024-07-01 13:23:42 +0200

    [ADD] sale_order_basket_number: Basket fields

---
+++ sale_order_basket_number/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale Order Basket Number",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "product",
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+        "views/sale_order_line_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}

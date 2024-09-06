b16f964c3d93712b3b86f94eb5dafd6fd8100c96
Unai Beristain <unaiberis@gmail.com>
2024-09-06 10:36:07 +0200

    [ADD] urbide_queries_2

---
+++ urbide_queries_2/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Urbide Queries 2",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Executes custom SQL queries during module installation and update",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "post_init_hook": "post_init_hook",
+}
